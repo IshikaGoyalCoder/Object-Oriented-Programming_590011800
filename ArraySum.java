eac37eeb8508d451300b08e76e7ac7958c1f233d
IshikaGoyalCoder <Ishika.Goyal575@gmail.com>
2026-02-02 09:26:08 +0530

    lab

---
+++ ArraySum.java
@@ -0,0 +1,15 @@
+
+// 4. WAP to print sum of array .  
+import java.util.Scanner;  class ArraySum {     
+    public static void main(String[] args) {         
+        Scanner sc = new Scanner(System.in);          
+        int n, sum = 0;         
+        System.out.print("Enter size of array: ");         
+        n = sc.nextInt();          int arr[] = new int[n];          
+        System.out.println("Enter array elements:");         
+        for (int i = 0; i < n; i++) {             
+            arr[i] = sc.nextInt();             
+            sum = sum + arr[i];         }          
+            System.out.println("Sum of array elements = " + sum);     
+        } 
+    }
