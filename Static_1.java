39fa7a146fc2cef99649eff96cba046896393374
IshikaGoyalCoder <Ishika.Goyal575@gmail.com>
2026-02-05 20:55:15 +0530

    lab

---
+++ Static_1.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+class Static_1 {
+   int n;
+
+   Static_1() {
+   }
+
+   public static void main(String[] var0) {
+      Scanner var1 = new Scanner(System.in);
+      Static_1 var2 = new Static_1();
+      var2.n = var1.nextInt();
+      System.out.println(var2.n);
+   }
+}
